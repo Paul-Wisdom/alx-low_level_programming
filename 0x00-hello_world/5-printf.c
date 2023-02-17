3df98c62f5fd54d654340346bed34877255059f3
Paul-Wisdom <wisdomp837@gmail.com>
2023-02-16 17:37:12 -0800

    Mandatory Tasks

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - prints a line of text
+ * Return: 0 whem compilation is succesful
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
