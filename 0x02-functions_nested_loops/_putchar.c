e549b7ae3620dfd2158e0b4a7a29d637e9dc971a
Paul-Wisdom <wisdomp837@gmail.com>
2023-02-21 13:10:22 -0800

    Task 1

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -5,18 +5,3 @@ int _putchar(char c)
 {
 	putchar(c);
 }
-
-int main(void)
-{
-	char *c = "Hello";
-
-	_putchar('y');
-	_putchar('\n');
-	while (*c)
-	{
-		_putchar(*c);
-		c++;
-	}
-	_putchar('\n');
-	return 0;
-}
