41f761cb48e73f3dda286fe6b007c1b6b78132b4
Paul-Wisdom <wisdomp837@gmail.com>
2023-02-21 13:43:29 -0800

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print the alphabet 10x
+ */
+void print_alphabet_x10(void)
+{
+	char ch;
+	int i = 0;
+
+	while (i < 10)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+	}
+}
