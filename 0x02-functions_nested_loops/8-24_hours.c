89a6224c9293c0d1a91f4f988da0a6b75e3404d6
Paul-Wisdom <wisdomp837@gmail.com>
2023-02-21 16:15:21 -0800

    Task

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,19 +10,13 @@ void jack_bauer(void)
 
 	for (i = 0; i < 24; i++)
 	{
-		if (i < 10)
-		{
-			_putchar('0');
-		}
-		_putchar(i + '0');
-		_putchar(':');
 		for (j = 0; j < 60; j++)
 		{
-			if (j < 10)
-			{
-				_putchar('0');
-			}
-			_putchar(j + '0');
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(':');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
 			_putchar('\n');
 		}
 	}
