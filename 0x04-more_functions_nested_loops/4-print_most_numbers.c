904866bbf2a79d87487c3cd0a28d465987a4fa72
Paul-Wisdom <wisdomp837@gmail.com>
2023-02-23 08:44:53 -0800

    Task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print numbers except 2 and 4
+ */
+void print_most_numbers(void)
+{
+	char ch = 0;
+
+	if ((ch != 2) || (ch != 4))
+	{
+		_putchar(ch);
+	}
+	else
+	{
+	}
+}
