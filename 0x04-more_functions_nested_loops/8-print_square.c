5c2683ee01177e33ac943795ccdf23f8563243d9
Paul-Wisdom <wisdomp837@gmail.com>
2023-02-23 17:31:35 -0800

    Task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_square - literally
+ * @size: length of a side
+ */
+void print_square(int size)
+{
+	int i = 0, j;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+	else if ((size == 0) || (size < 0))
+		_putchar('\n');
