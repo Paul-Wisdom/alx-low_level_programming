d453149999b9b78025d3ca94477bdff3e66477bf
Paul-Wisdom <wisdomp837@gmail.com>
2023-03-02 00:06:04 -0800

    Tasks

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * reverse_array - literally
+ * @a: array
+ * @n: array size
+ */
+void reverse_array(int *a, int n)
+{
+	int i, temp;
+
+	for (i = n - 1; i >= (n / 2); i--)
+	{
+		temp = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = temp;
+	}
