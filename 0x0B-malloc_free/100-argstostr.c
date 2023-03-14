35210eb60d26dff983e69efb8c68e60cf2e85570
Paul-Wisdom <wisdomp837@gmail.com>
2023-03-14 01:30:51 -0700

    advanced tasks

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * argstostr - concatenates arguments of a program
+ * @ac: num of args
+ * @av: args
+ * Return: char array
+ */
+char *argstostr(int ac, char **av)
+{
+	char *p;
+	int i, j, l, buff;
+
+	i = j = j = buf = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (i = 0; av[i]; i++)
+		for (j = 0; av[i][j]; j++)
+			l++;
+	p = (char *)malloc((l * sizeof(char)) + ac + 1);
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; av[i]; i++)
+	{
+		for (j = 0; av[i][j]; j++, buff++)
+			p[buff] = av[i][j];
+		p[buff] = '\n';
+		buff++;
+	}
+	p[buff] = '\0';
+	return (p);
+}
