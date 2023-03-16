3af065b253291f7e21d40d4167cfc885c2f6756f
Paul-Wisdom <wisdomp837@gmail.com>
2023-03-16 04:21:44 -0700

    Tasks

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,9 +14,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *p;
 
 	if (s1 == NULL)
-		s1 = '';
+		s1 = "";
 	if (s2 == NULL)
-		s2 = '';
+		s2 = "";
 	while (s1[len1] != '\0')
 		len1++;
 	while (s2[len2] != '\0')
