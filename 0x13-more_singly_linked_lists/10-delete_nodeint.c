06e70e79679ba457d82b2a5dc650fc1a6c02c50b
Paul-Wisdom <wisdomp837@gmail.com>
2023-04-04 17:11:46 -0700

    betty

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -6,14 +6,14 @@
  */
 size_t listint_len(const listint_t *h)
 {
-        size_t i = 0;
+	size_t i = 0;
 
-        while (h != NULL)
-        {
-                i++;
-                h = h->next;
-        }
-        return (i);
+	while (h != NULL)
+	{
+		i++;
+		h = h->next;
+	}
+	return (i);
 }
 /**
  * delete_nodeint_at_index - literally
