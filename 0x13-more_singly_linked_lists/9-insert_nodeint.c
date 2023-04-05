2d10ec6dface58b39d95f03676a14f75f7710e78
Paul-Wisdom <wisdomp837@gmail.com>
2023-04-04 17:20:00 -0700

    t

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,7 +13,7 @@ size_t listint_len(const listint_t *h)
 		i++;
 		h = h->next;
 	}
-	 return (i);
+	return (i);
 }
 /**
  * add_nodeint - adds node at beginning of list
@@ -23,17 +23,17 @@ size_t listint_len(const listint_t *h)
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-        listint_t *new;
+	listint_t *new;
 
-        new = (listint_t *)malloc(sizeof(listint_t));
-        if (!new)
-        {
-                return (NULL);
-        }
-        new->n = n;
-        new->next = *head;
-        *head = new;
-        return (new);
+	new = (listint_t *)malloc(sizeof(listint_t));
+	if (!new)
+	{
+		return (NULL);
+	}
+	new->n = n;
+	new->next = *head;
+	*head = new;
+	return (new);
 }
 /**
  * insert_nodeint_at_index - literally
