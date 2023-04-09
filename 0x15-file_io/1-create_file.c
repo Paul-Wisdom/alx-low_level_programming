18fc5215524c8e531bb6b5a22d88e4c5c2dc6b16
Paul-Wisdom <wisdomp837@gmail.com>
2023-04-09 15:44:04 -0700

    testing

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * create_file - a func that creates a file
+ * @filename: filename
+ * @text_content: the content written into the file
+ * Return: 1 on success and -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int o, w, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len]; )
+			len++;
+	}
+
+	o = open(filename, O_RDWR | O_CREAT | O_TRUNC, 600);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
+		return (-1);
+	close (o);
+	return (1);
+}
